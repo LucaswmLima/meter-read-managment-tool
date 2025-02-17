ac189e2c0141f659a5c8783df9660e007cda062c
Lucas William <lucaswilliamml@gmail.com>
2025-02-17 19:19:44 -0300

    added a fallback url

--- client/src/api/confirmRoute.ts
+++ client/src/api/confirmRoute.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-//const BASE_URL = "http://localhost:3000";
-const BASE_URL = import.meta.env.API_URL;
+const BASE_URL = import.meta.env.API_URL || "http://localhost:3000";
 
 const confirmRoute = async (uuid: string, confirmedValue: number) => {
   const response = await axios.patch(`${BASE_URL}/confirm/`, {
--- client/src/api/listRoute.ts
+++ client/src/api/listRoute.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-//const BASE_URL = "http://localhost:3000";
-const BASE_URL = import.meta.env.API_URL;
+const BASE_URL = import.meta.env.API_URL || "http://localhost:3000";
 
 const addRoute = async (customerCode: string) => {
+  console.log(`${BASE_URL}${customerCode}/list/`);
+  
   const response = await axios.get(
     `${BASE_URL}${customerCode}/list/`,
     {}
